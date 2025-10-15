db98e4891c80f35432ae6e75dc3d58cd18e62503
Clément Oliva <clement.oliva@gmail.com>
2025-10-15 15:28:51 +0200

    Add some plugins

--- lua/plugins/coloscheme.lua
+++ lua/plugins/coloscheme.lua
@@ -1,23 +1,32 @@
 return {
-    -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
+  -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  'folke/tokyonight.nvim',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require('tokyonight').setup {
+      styles = {
+        comments = { italic = false }, -- Disable italics in comments
+      },
+    }
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    vim.cmd.colorscheme 'tokyonight-night'
+
+    -- Commande pour passer en "white mode"
+    vim.api.nvim_create_user_command("WhiteMode", function()
+      vim.cmd.colorscheme 'tokyonight-day'
+    end, {})
+
+    -- Commande pour revenir au thème sombre
+    vim.api.nvim_create_user_command("DarkMode", function()
       vim.cmd.colorscheme 'tokyonight-night'
-    end,
+    end, {})
+  end,
 }
-
