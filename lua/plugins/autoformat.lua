53a6fb768c28f956ffdf8081faafefcf74e07590
Clément Oliva <clement.oliva@gmail.com>
2026-02-21 18:15:22 +0100

    JSON Formatter

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -31,6 +31,7 @@ return { -- Autoformat
     formatters_by_ft = {
       lua = { 'stylua' },
       rust = { 'rustfmt', lsp_format = 'fallback' },
+      json = { "prettier" },
       -- ADD THESE 👇
       -- css = { 'prettier' },
       -- scss = { 'prettier' },
