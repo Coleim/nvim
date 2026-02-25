a9e9e9704aeaf0bf4e5fe32ea2d4aae7e9965ad4
Clement Oliva <clement.oliva@gmail.com>
2026-02-25 11:43:29 +0100

    Refactor typescript-tools configuration for improved organization and functionality

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -1,24 +1,26 @@
 return {
   "pmizio/typescript-tools.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {
-    expose_as_code_action = 'all',
-    -- Disable formatting from typescript-tools to avoid conflicts with Prettier
-    on_attach = function(client, bufnr)
-      client.server_capabilities.documentFormattingProvider = false
-      client.server_capabilities.documentRangeFormattingProvider = false
+  ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+  config = function()
+    require("typescript-tools").setup({
+      expose_as_code_action = "all",
+      on_attach = function(client, bufnr)
+        -- Disable formatting from typescript-tools to avoid conflicts with Prettier
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
 
-      -- Auto organize imports and remove unused on save (runs before Prettier)
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        buffer = bufnr,
-        callback = function()
-          local api = require("typescript-tools.api")
-          api.organize_imports(true)   -- true = sync
-          api.remove_unused(true)      -- true = sync
-        end,
-      })
-    end,
-  },
+        -- Auto remove unused imports on save (runs before Prettier)
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          buffer = bufnr,
+          callback = function()
+            local api = require("typescript-tools.api")
+            api.remove_unused(true) -- true = sync
+          end,
+        })
+      end,
+    })
+  end,
   keys = {
     { "<leader>oi", "<cmd>TSToolsOrganizeImports<cr>", desc = "Organize Imports" },
     { "<leader>ru", "<cmd>TSToolsRemoveUnused<cr>", desc = "Remove Unused" },
