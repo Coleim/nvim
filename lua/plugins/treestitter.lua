f8245ef13866d08fb2462ec5f06e810a632c4079
Clément Oliva <clement.oliva@gmail.com>
2026-06-26 15:56:39 +0200

    Handle 'old' versions of treesitter

--- lua/plugins/treestitter.lua
+++ lua/plugins/treestitter.lua
@@ -1,24 +1,48 @@
-return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  opts = {
-    ensure_installed = {
-      'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline',
-      'query', 'vim', 'vimdoc', 'javascript', 'typescript', 'tsx', 'css', 'json',
-      'yaml', 'toml', 'rust',
-    },
-  },
-  config = function(_, opts)
-    require('nvim-treesitter').setup(opts)
+local use_master = vim.env.NVIM_TS_MASTER == '1'
 
-    -- Install missing parsers immediately
-    require('nvim-treesitter.install').install(opts.ensure_installed, { summary = false })
-
-    vim.api.nvim_create_autocmd({ 'FileType', 'BufEnter' }, {
-      pattern = { 'typescriptreact', 'javascriptreact', 'typescript', 'javascript' },
-      callback = function(ev)
-        pcall(vim.treesitter.start, ev.buf)
-      end,
-    })
-  end,
+local ensure_installed = {
+  'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline',
+  'query', 'vim', 'vimdoc', 'javascript', 'typescript', 'tsx', 'css', 'json',
+  'yaml', 'toml', 'rust',
 }
+
+if use_master then
+  return {
+    'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
+    build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = ensure_installed,
+        auto_install = true,
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  }
+else
+  return {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    opts = {
+      ensure_installed = ensure_installed,
+    },
+    config = function(_, opts)
+      require('nvim-treesitter').setup(opts)
+
+      -- Install missing parsers immediately
+      require('nvim-treesitter.install').install(opts.ensure_installed, { summary = false })
+
+      vim.api.nvim_create_autocmd({ 'FileType', 'BufEnter' }, {
+        pattern = { 'typescriptreact', 'javascriptreact', 'typescript', 'javascript' },
+        callback = function(ev)
+          pcall(vim.treesitter.start, ev.buf)
+        end,
+      })
+    end,
+  }
+end
